cb558aa4dcaeaa67860b68b6868a33c48e31f7e3
scripter-kartik <agarwalkartik704@gmail.com>
2026-08-29 18:17:38 +0530

    chore: clean up temporary script files

--- scratch-youtubei.js
+++
@@ -1,8 +0,0 @@
-const { Innertube } = require('youtubei.js');
-async function test() {
-  const yt = await Innertube.create();
-  const info = await yt.getInfo('jNQXAC9IVRw'); // using getInfo instead of getBasicInfo
-  const format = info.chooseFormat({ type: 'audio', quality: 'best' });
-  console.log(format ? format.url : "no format");
-}
-test().catch(console.error);
--- scratch-ytdl.js
+++
@@ -1,7 +0,0 @@
-const ytdl = require('@distube/ytdl-core');
-async function test() {
-  const info = await ytdl.getInfo('jNQXAC9IVRw');
-  const format = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
-  console.log(format.url);
-}
-test().catch(console.error);
